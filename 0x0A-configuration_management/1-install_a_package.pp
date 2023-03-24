3cda10692d603a494d7d7c2ad5c01b25143b2a99
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-24 22:07:52 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Manifest that installs puppet-lint version 2.5.0
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# install flask from pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
